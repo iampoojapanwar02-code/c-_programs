ca010298f6789c68b5f0296bcc997d0b44b67beb
iampoojapanwar02-code <iampoojapanwar02@gmail.com>
2026-08-21 10:41:41 +0530

    add basic programs

---
+++ basic_programs/area_of_rectangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int breadth,length,area;
+	cout<<"Enter breadth of rectangle:";
+	cin>>breadth;
+	cout<<"Enter length of rectangle :";
+	cin>>length;
+	area=length*breadth;
+	cout<<"Area of reactangle is" << area;
+	return 0;
+}
