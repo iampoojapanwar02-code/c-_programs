4721942f47f172879e80ecac02fdbced30a493b1
iampoojapanwar02-code <iampoojapanwar02@gmail.com>
2026-08-21 10:39:54 +0530

    add basic programs

---
+++ basic_programs/division.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2,division;
+	cout<<"Enter n1:";
+	cin>>n1;
+	cout<<"Enter n2:";
+	cin>>n2;
+	division=n1/n2;
+	cout<<"Multiple of n1 and n2 is" <<division;
+	return 0;
+}
