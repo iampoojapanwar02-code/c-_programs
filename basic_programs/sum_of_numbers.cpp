37bf88ce2cfc19066805b1244c5b4576236f8d36
iampoojapanwar02-code <iampoojapanwar02@gmail.com>
2026-08-21 10:20:01 +0530

    add basic programs

---
+++ basic_programs/sum_of_numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2,sum;
+	cout<<"Enter n1:";
+	cin>>n1;
+	cout<<"Enter n2:";
+	cin>>n2;
+	sum=n1+n2;
+	cout<<"Sum of n1 and n2 is"<<sum;
+}
