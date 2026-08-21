e29fcddb8c9c66d6500f0bf5562292eaee21a71c
iampoojapanwar02-code <iampoojapanwar02@gmail.com>
2026-08-21 10:38:41 +0530

    add basic programs

---
+++ basic_programs/multiple.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2,multi;
+	cout<<"Enter n1:";
+	cin>>n1;
+	cout<<"Enter n2:";
+	cin>>n2;
+	multi=n1*n2;
+	cout<<"Multiple of n1 and n2 is" <<multi;
+	return 0;
+}
