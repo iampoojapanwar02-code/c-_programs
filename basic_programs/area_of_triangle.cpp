84d1ccaab00e133d388cc126aa537dd5a1f970bf
iampoojapanwar02-code <iampoojapanwar02@gmail.com>
2026-08-21 10:42:22 +0530

    add basic programs

---
+++ basic_programs/area_of_triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int base,height,area;
+	cout<<"Enter base of triangle:";
+	cin>>base;
+	cout<<"Enter height of triangle :";
+	cin>>height;
+	area=0.5*base*height;
+	cout<<"Area of triangle is" <<area;
+	return 0;
+}
