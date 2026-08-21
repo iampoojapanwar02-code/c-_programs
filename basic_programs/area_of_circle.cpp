4153c361ea90b1a6c718b7f9907c06b1564caf0b
iampoojapanwar02-code <iampoojapanwar02@gmail.com>
2026-08-21 10:41:01 +0530

    add basic programs

---
+++ basic_programs/area_of_circle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int radius,area;
+	cout<<"Enter radius of circle:";
+	cin>>radius;
+	
+	area=radius*radius*3.14;
+	cout<<"Area of circle is" << area;
+	
+	return 0;
+}
